ee4fd64f639be10c36cb6b0840dc82ce1fd5dc30
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-17 16:43:50 -0600

    added quiz card generation, right and wrong answers and question generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,29 +36,28 @@ var card1 = {
     id: 1,
     question: "this is the question for card id=1",
     wrongAnswers: ["card 1: wrong answer 1", "card 1: wrong answer 2", "card 1: wrong answer 3"],
-    correctAnswer: "card 1: correct answer",
+    rightAnswer: "card 1: right answer",
 };
 var card2 = {
     id: 2,
     question: "this is the question for card id=2",
     wrongAnswers: ["card 2: wrong answer 1", "card 2: wrong answer 2", "card 2: wrong answer 3"],
-    correctAnswer: "card 2: correct answer",
+    rightAnswer: "card 2: right answer",
 };
 var card3 = {
     id: 3,
     question: "this is the question for card id=3",
     wrongAnswers: ["card 3: wrong answer 1", "card 3: wrong answer 2", "card 3: wrong answer 3"],
-    correctAnswer: "card 3: correct answer",
+    rightAnswer: "card 3: right answer",
 };
 var card4 = {
     id: 4,
     question: "this is the question for card id=4",
     wrongAnswers: ["card 4: wrong answer 1", "card 4: wrong answer 2", "card 4: wrong answer 3"],
-    correctAnswer: "card 4: correct answer",
+    rightAnswer: "card 4: right answer",
 };
 var quizCardArray = [card1, card2, card3, card4];
 shuffleArray(quizCardArray);
-
 
 // GLOBAL FUNCTIONS
 // Prompt to begin
@@ -79,7 +78,6 @@ var displayMessage = function(msgObj) {
         var btnMsg = msgObj.btnMsg;
 
         var currentCard = document.querySelector(".msg-card");
-        var msgEl = document.querySelector(".display");
         currentCard.remove();
 
         var newCard = document.createElement("article");
@@ -90,7 +88,7 @@ var displayMessage = function(msgObj) {
         newBtn.textContent = btnMsg;
 
         newCard.appendChild(newBtn);
-        msgEl.appendChild(newCard);
+        displayEl.appendChild(newCard);
     }
     else if (msgObj === timesUpMsgObj) {
         var displayMsg = msgObj.msg;
@@ -100,10 +98,9 @@ var displayMessage = function(msgObj) {
         var btnNo = msgObj.btnMsgN;
 
         var currentCard = document.querySelector(".msg-card");
-        var msgEl = document.querySelector(".display");
         currentCard.remove();
 
-        var newCard = msgEl.createElement("article");
+        var newCard = displayEl.createElement("article");
         newCard.className = "msg-card";
         newCard.innerHTML = displayMsg;
         var newBtnY = document.createElement("button")
@@ -120,6 +117,9 @@ var startTimer = function (event) {
     var targetBtn = event.target;
 
     if (targetBtn.matches("#start")) {
+        var currentCard = document.querySelector(".msg-card");
+        currentCard.remove();
+
         runQuiz();
         
         let currentTime = 60;
@@ -138,8 +138,57 @@ var startTimer = function (event) {
 
 // Runs through quiz cards
 var runQuiz = function() {
+    for (var i = 0; i < quizCardArray; i++) {
+        var currentCard = quizCardArray[i];
+        var wrongAnswers = currentCard.wrongAnswers;
+        var rightAnswer = currentCard.rightAnswer;
 
-}
+        var newCardEl = displayEl.createElement("article");
+        newCardEl.className = "quiz-card";
+        newCardEl.setAttribute("id", currentCard.id);
+
+        var cardQuestion = displayEl.createElement("h4");
+        cardQuestion.className = "quiz-question";
+        cardQuestion.textContent = currentCard.question;
+        newCardEl.appendChild(cardQuestion);
+
+        var cardAnswersEl = displayEl.createElement("ul");
+        cardAnswersEl.className = "quiz-answers";
+        var allAnswers = [];
+
+        for (var i = 0; i < wrongAnswers.length; i++) {
+            var wrongAnswerEl = displayEl.createElement("li");
+            var wrongAnswerBtn = displayEl.createElement("button");
+            wrongAnswerBtn.setAttribute("data-answer", 0);
+            wrongAnswerBtn.textContent = wrongAnswers[i];
+            wrongAnswerEl.appendChild(wrongAnswerBtn);
+
+            allAnswers.push(wrongAnswerEl);
+        }
+
+        var rightAnswerEl = displayEl.createElement("li");
+        var rightAnswerBtn = displayEl.createElement("button");
+        rightAnswerBtn.setAttribute("data-answer", 1);
+        rightAnswerBtn.textContent = rightAnswer;
+        rightAnswerEl.appendChild(rightAnswerBtn);
+
+        allAnswers.push(rightAnswerEl);
+        shuffleArray(allAnswers);
+
+        for (var i = 0; i < allAnswers.length; i++) {
+            cardAnswersEl.appendChild(allAnswers[i]);
+        }
+
+        newCardEl.appendChild(cardAnswersEl);
+
+        currentCardHandler(currentCard);
+    }
+};
+
+var currentCardHandler = function(card) {
+    var currentCard = card;
+
+;}
 
 var playAgain = function(event) {
     var targetBtn = event.target;
